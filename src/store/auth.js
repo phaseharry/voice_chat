5093464df5f15d4e73541083b1d0e9bd8b47f31e
Harry Chen <harrychenhc@icloud.com>
2018-11-10 15:34:12 -0500

    adf

--- src/store/auth.js
+++ src/store/auth.js
@@ -1,6 +1,14 @@
 
+const initialState = {
+  "_id": 4,
+  "createdAt": "2018-11-10T19:55:29.850Z",
+  "email": "mike@gmail.com",
+  "name": "Mikey Smikey",
+  "password": "mike", 
+  "updatedAt": "2018-11-10T19:55:29.850Z",
+}
 
-const authReducer = (state = {}, action) => {
+const authReducer = (state = initialState, action) => {
   switch(action.type){
     default:
       return state
